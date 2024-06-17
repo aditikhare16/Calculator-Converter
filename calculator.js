181fa7c2cfbe78833fa17bda77b8e0aa77645bfe
Aditi Khare <114524718+aditikhare16@users.noreply.github.com>
2024-06-17 17:28:58 +0530

    Update calculator.js for Dark Mode

--- calculator.js
+++ calculator.js
@@ -23,3 +23,22 @@ function calculate() {
         document.getElementById('calculator-display').value = 'Error';
     }
 }
+(function() {
+    const toggleButton = document.querySelector('.dark-mode-toggle');
+    const currentTheme = localStorage.getItem('theme');
+    
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+    }
+
+    toggleButton.addEventListener('click', function() {
+        let theme = document.documentElement.getAttribute('data-theme');
+        if (theme === 'dark') {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+})();
