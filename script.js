9a9dcfa6958819872302debf37868fbe281bd72b
Aditi Khare <114524718+aditikhare16@users.noreply.github.com>
2024-06-17 17:27:53 +0530

    Update script.js for dark mode

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ document.querySelector('.option-card:nth-child(2)').addEventListener('click', fu
 function openPage(page) {
     window.location.href = page;
 }
+// dark mode
+(function() {
+    const toggleButton = document.querySelector('.dark-mode-toggle');
+    const currentTheme = localStorage.getItem('theme');
+    
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+    }
+
+    toggleButton.addEventListener('click', function() {
+        let theme = document.documentElement.getAttribute('data-theme');
+        if (theme === 'dark') {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+})();
