b664f61532bdab6da2e3a7eeb9ecc04e9bb380fc
Aditi Khare <114524718+aditikhare16@users.noreply.github.com>
2024-06-17 17:28:27 +0530

    Update converters.js for dark mode

--- converters.js
+++ converters.js
@@ -66,3 +66,22 @@ function convertTemperature() {
 
     document.getElementById('output-temperature').textContent = `Converted Temperature: ${convertedTemperature.toFixed(2)} ${toUnit}`;
 }
+(function() {
+    const toggleButton = document.querySelector('.dark-mode-toggle');
+    const currentTheme = localStorage.getItem('theme');
+    
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+    }
+
+    toggleButton.addEventListener('click', function() {
+        let theme = document.documentElement.getAttribute('data-theme');
+        if (theme === 'dark') {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+})();
